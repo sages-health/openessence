d117959c58073e6068f0a3797c825496106b2f2e
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2013-11-12 16:04:55 -0500

    Squashed 'app/kibana/' content from commit 22040c7
    git-subtree-dir: app/kibana
    git-subtree-split: 22040c7dd1a35ef55184f6b5e44406ff45ff3461

---
+++ src/app/directives/all.js
@@ -0,0 +1,12 @@
+define([
+  './addPanel',
+  './arrayJoin',
+  './dashUpload',
+  './kibanaPanel',
+  './kibanaSimplePanel',
+  './ngBlur',
+  './ngModelOnBlur',
+  './tip',
+  './confirmClick',
+  './esVersion'
+], function () {});
