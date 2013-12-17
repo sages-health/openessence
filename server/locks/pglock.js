680c8683bc57eebd0de521b25a044dcb7a886936
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2013-12-17 15:43:40 -0500

    Lessons learned with PG advisory locks

--- server/locks/pglock.js
+++ server/locks/pglock.js
@@ -1,5 +1,28 @@
 'use strict';
 
+/**
+ * Locking with PostgreSQL session-level advisory locks.
+ *
+ * PG session-level advisory locks are automatically released
+ * when a session ends. This means deadlock can happen if a session doesn't end. This can happen if a connection
+ * is terminated improperly, e.g. because there was an uncaught, unrecoverable exception. (Node is very good about
+ * closing TCP connections on process.exit. However, it sometimes fails to if the process dies because of an
+ * unrecoverable error, e.g. a ReferenceError). If client dies suddenly while holding an advisory lock, the session
+ * can get stuck in an "idle in transaction" state. To mitigate this, proxy connections through pgBouncer
+ * (which you should be doing anyway) and set idle_transaction_timeout (available in pgBouncer 1.5+). For more info,
+ * see http://stackoverflow.com/a/13245020.
+ *
+ * If you do encounter deadlock from an idle transaction, the only thing you can do (I think) is kill the backend.
+ * It would be nice if you could just unlock the held lock, but since you're unlocking it from a different session,
+ * it will have no effect.
+ *
+ * You can kill all backends with an outstanding advisory lock with
+ *
+ *     select pg_terminate_backend(pid) from
+ *     (select pid from pg_locks where locktype = 'advisory' and granted = true) t.
+ *
+ */
+
 var _ = require('lodash');
 var crc32 = require('buffer-crc32');
 var pg = require('pg');
