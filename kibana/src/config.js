73b5aa94962e869cc71fae23ad94687d2273a918
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-01-03 16:41:07 -0500

    Turn elasticsearch proxying on

--- kibana/src/config.js
+++ kibana/src/config.js
@@ -21,7 +21,7 @@ function (Settings) {
      * kibana installed on. You probably want to set it to the FQDN of your
      * elasticsearch host
      */
-    elasticsearch: "http://"+window.location.hostname+":9200",
+    elasticsearch: '//' + window.location.host + '/es',//"http://"+window.location.hostname+":9200",
 
     /** @scratch /configuration/config.js/5
      * ==== default_route
