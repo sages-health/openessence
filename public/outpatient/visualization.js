0b3ad02a6399b3ae995e340e056c2e06b414ed27
Charles Hodanics <charles.hodanics@jhuapl.edu>
2014-05-12 15:22:09 -0400

    Pie and bar charts in workbench

--- public/outpatient/visualization.js
+++ public/outpatient/visualization.js
@@ -3,10 +3,7 @@
 var angular = require('angular');
 var directives = require('../scripts/modules').directives;
 
-angular.module(directives.name).directive('outpatientVisualization', function ($http, $modal, orderByFilter,
-                                                                               gettextCatalog, sortString, FrableParams,
-                                                                               OutpatientVisit, outpatientEditModal,
-                                                                               outpatientDeleteModal) {
+angular.module(directives.name).directive('outpatientVisualization', function ($http, $modal, orderByFilter, gettextCatalog, sortString, FrableParams, OutpatientVisit, outpatientEditModal, outpatientDeleteModal, outpatientAggregation) {
   return {
     restrict: 'E',
     template: require('./visualization.html'),
@@ -17,15 +14,33 @@ angular.module(directives.name).directive('outpatientVisualization', function ($
     },
     link: {
       // runs before nested directives, see http://stackoverflow.com/a/18491502
-      pre: function (scope) {
+      pre: function (scope, element) {
+
         scope.visualization = {
           name: 'table'
+        };
+
+        scope.aggData = [
+          //{ key: 'One',   value: Math.floor(Math.random()*20) } -- pie
+          //{ key: 'Series', values:[{ key: 'One',   value: Math.floor(Math.random()*20) }]} -- bar
+        ];
+
+        scope.xFunction = function () {
+          return function (d) {
+            return d.key;
+          };
+        };
+        scope.yFunction = function () {
+          return function (d) {
+            return d.value;
+          };
         };
 
         scope.pivot = {
           rows: [],
           cols: []
         };
+
         scope.pivotOptions = [
           {
             value: 'sex',
@@ -34,6 +49,10 @@ angular.module(directives.name).directive('outpatientVisualization', function ($
           {
             value: 'age',
             label: gettextCatalog.getString('Age')
+          },
+          {
+            value: 'symptoms',
+            label: gettextCatalog.getString('Symptoms')
           }
         ];
 
@@ -46,9 +65,152 @@ angular.module(directives.name).directive('outpatientVisualization', function ($
           edit: gettextCatalog.getString('Edit')
         };
 
+//        var d3_category20 = [ "#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5" ];
+//        var d3_category20b = [ "#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b", "#cedb9c", "#8c6d31", "#bd9e39", "#e7ba52", "#e7cb94", "#843c39", "#ad494a", "#d6616b", "#e7969c", "#7b4173", "#a55194", "#ce6dbd", "#de9ed6" ];
+//        var d3_category20c = [ "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476", "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9" ];
+//
+//        scope.colorFunction = function() {
+//          return function(d, i) {
+//            return d3_category20[i % 20];
+//          };
+//        }
+
+        var buildAggregation = function (field) {
+          var agg = {};
+          agg[field] = outpatientAggregation.getAggregation(field, 10);
+          return agg;
+        };
+
+        //assuming only two deep for now..
+        var buildAggregationQuery = function (cols, rows) {
+          var query, first, second;
+          if (cols[0]) {
+            first = cols[0];
+            if (rows[0]) {
+              second = rows[0];
+            }
+          } else if (rows[0]) {
+            first = rows[0];
+          }
+          //build first aggregation
+          if (first) {
+            query = buildAggregation(first);
+            //if a second exists, add to first aggregation object
+            if (second) {
+              query[first]['aggs'] = buildAggregation(second);
+            }
+          }
+          return query;
+        };
+
+        //assuming only two deep BY one for now...
+        var parseAggQuery = function (aggregation, cols, rows) {
+          var aggs = aggregation.aggregations;
+
+          var col = cols[0];
+          var colLabel = scope.strings[col] || col;
+
+          var row = rows[0];
+          var rowLabel = scope.strings[row] || row;
+
+          var pieData = []; //{ col: col, key: col, value: count }
+          var barData = []; //{ col: col, key: keyStr, values: [{col: col, key: keyStr, value: count}]}...
+          var missingCount = aggregation.total;
+          var slice;
+          if (aggs) {
+            //if there is only one aggregation selected parse as normal
+            if (col && !row) {
+              aggs[col].buckets.map(function (entry) {
+                var keyStr = outpatientAggregation.bucketToKey(entry);
+                missingCount -= entry.doc_count;
+                slice = {col: col, colName: keyStr, key: keyStr, value: entry.doc_count};
+                pieData.push(slice);
+                barData.push({col: col, colName: keyStr, key: keyStr, values: [slice]});
+              });
+              //add missing fields count from total hits - aggs total doc count
+              if (missingCount > 0) {
+                slice = {col: col, colName: 'missing', key: ('missing_' + col), value: missingCount};
+                pieData.push(slice);
+                barData.push({col: col, colName: 'missing', key: ('missing_' + col), values: [slice]});
+              }
+            }
+            if (!col && row) {
+              aggs[row].buckets.map(function (entry) {
+                var keyStr = outpatientAggregation.bucketToKey(entry);
+                missingCount -= entry.doc_count;
+                slice = {row: row, rowName: keyStr, key: keyStr, value: entry.doc_count};
+                pieData.push(slice);
+              });
+              //add missing fields count from total hits - aggs total doc count
+              if (missingCount > 0) {
+                slice = {row: row, rowName: 'missing', key: ('missing_' + rowLabel), value: missingCount};
+                pieData.push(slice);
+              }
+              barData.push({row: row, key: rowLabel, values: pieData});
+            }
+            //if there are two aggregations parse and return as agg1-agg2..
+            if (col && row) {
+              var missingTotalCount = aggregation.total;//aggs[cols[0]].buckets.doc_count;
+              aggs[col].buckets.map(function (entry) {
+                var keyStr = outpatientAggregation.bucketToKey(entry);
+                missingTotalCount -= entry.doc_count;
+                var missingCount = entry.doc_count;
+                var data = [];
+                entry[row].buckets.map(function (sub) {
+                  var subStr = outpatientAggregation.bucketToKey(sub);
+                  missingCount -= sub.doc_count;
+                  slice = {col: col, colName: entry.key, row: row, rowName: sub.key,
+                    key: (keyStr + '_' + subStr), value: sub.doc_count};
+                  data.push(slice);
+                  pieData.push(slice);
+                });
+                //add missing fields count from total hits - aggs total doc count
+                if (missingCount > 0) {
+                  slice = {col: col, colName: entry.key, row: row, rowName: 'missing',
+                    key: ('missing_' + keyStr + '_' + rowLabel), value: missingCount};
+                  data.push(slice);
+                  pieData.push(slice);
+                }
+                barData.push({key: keyStr, values: data});
+              });
+              //add missing fields count from total hits - aggs total doc count
+              if (missingTotalCount > 0) {
+                slice = {col: col, colName: 'missing', key: ('missing_' + colLabel), value: missingTotalCount};
+                barData.push({col: col, colName: 'missing', key: ('missing_' + colLabel), values: [slice]});
+                pieData.push(slice);
+              }
+            }
+            if (scope.visualization.name === 'bar') {
+              return barData;
+            } else if (scope.visualization.name === 'pie') {
+              return pieData;
+            }
+          }
+          return [];
+        };
+
+        var aggReload = function () {
+          var cols = angular.copy(scope.pivot.cols);
+          var rows = angular.copy(scope.pivot.rows);
+          //query the new data for aggregations
+          OutpatientVisit.search({
+            size: 0,
+            q: scope.queryString,
+            aggregations: buildAggregationQuery(cols, rows)
+          }, function (data) {
+            scope.aggData = parseAggQuery(data, cols, rows);
+          });
+        };
+
         var reload = function () {
           if (scope.visualization.name === 'table') {
             scope.tableParams.reload();
+          }
+          if (scope.visualization.name === 'pie') {
+            aggReload();
+          }
+          if (scope.visualization.name === 'bar') {
+            aggReload();
           }
         };
 
@@ -104,6 +266,35 @@ angular.module(directives.name).directive('outpatientVisualization', function ($
 
         scope.$watch('queryString', function () {
           reload();
+        });
+        scope.$watch('pivot.cols', function () {
+          reload();
+        });
+        scope.$watch('pivot.rows', function () {
+          reload();
+        });
+        scope.$watch('visualization.name', function () {
+          reload();
+        });
+
+        scope.$on('elementClick.directive', function (angularEvent, event) {
+          var filter;
+          if (event.point.col && !event.point.colName.startsWith('missing')) {
+            filter = {
+              type: event.point.col,
+              value: event.point.colName
+            };
+            //scope.$apply(function(){scope.filters.push(filter);});
+            scope.$emit('aggClick', filter, true);
+          }
+          if (event.point.row && !event.point.rowName.startsWith('missing')) {
+            filter = {
+              type: event.point.row,
+              value: event.point.rowName
+            };
+            //scope.$apply(function(){scope.filters.push(filter);});
+            scope.$emit('aggClick', filter, true);
+          }
         });
       }
     }
