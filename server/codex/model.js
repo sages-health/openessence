b1a54cdd39fc38acdcc71ad3397ce4eb9fbdbef1
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-06-17 10:39:46 -0400

    Overhaul Codex, again

--- server/codex/model.js
+++ server/codex/model.js
@@ -1,155 +1,263 @@
 'use strict';
 
 var _ = require('lodash');
-var changeCase = require('change-case');
-var errors = require('./errors');
-var Index = require('./es-index');
+var async = require('async');
 
-function Model (options) {
-  options = _.assign({
-    // we use param-case since that's the de facto REST standard for resources, even if elasticsearch uses snake_case
-    index: this.constructor.INDEX || changeCase.paramCase(this.constructor.name)
-  }, options);
+function model (modelOptions) {
+  var index = modelOptions.index;
+  var type = modelOptions.type;
+  var refresh = modelOptions.refresh;
+  var client = modelOptions.client || require('./client');
 
-  this.type = options.type || this.constructor.TYPE || this.index;
-  this.client = options.client || require('./client'); // delay instantiating client
-  this.index = _.isString(options.index) ? new Index(options.index, this.client) : options.index;
+  /**
+   *
+   * @param doc document to index, potentially untrusted
+   * @param instanceOptions options affecting this singular model instance
+   * @constructor
+   * @private
+   */
+  var Model = function Model_ (doc, instanceOptions) {
+    if (!(this instanceof Model_)) {
+      return new Model_(doc, instanceOptions);
+    }
+
+    // NOTE: doc may come from the client, so don't trust it
+
+    instanceOptions = instanceOptions || {};
+
+    var getSetting = function (setting) {
+      return instanceOptions[setting] || modelOptions[setting];
+    };
+
+    _.assign(this, doc);
+
+    // Add properties to instance in case any consumers need them
+    Object.defineProperty(this, '_', {
+      enumerable: false, // separate regular fields from document fields
+      configurable: false,
+      writable: false,
+      value: {
+        // this might be useful if each model instance can override index or type
+        index: getSetting('index') || instanceOptions._index,
+        type: getSetting('type') || instanceOptions._type,
+
+        // these fields are different for every model instance
+        id: getSetting('id') || instanceOptions._id,
+        version: getSetting('version') || instanceOptions._version,
+
+        refresh: getSetting('refresh'),
+        client: getSetting('client'),
+
+        // add class methods to instance for convenience
+        get: Model.get,
+        search: Model.search,
+        delete: Model.delete
+      }
+    });
+  };
+
+  // give consumers enough information so they can use the client directly
+  Model.index = index;
+  Model.type = type;
+  Model.client = client;
+
+  // Model.with works in all modern environments, but since `with` is a reserved word, some IDEs complain
+  Model['with'] = function (plugin) {
+    // this is an initialization function, so it's OK to be synchronous
+    return plugin(Model) || Model; // plugins don't have to return the Model if they mutate it
+  };
+
+  var initHandler = function (name, defaultFunc) {
+    if (Array.isArray(modelOptions[name])) {
+      // callers can pass an array to override default handlers
+      Model[name] = modelOptions[name];
+    } else {
+      Model[name] = [defaultFunc];
+      if (modelOptions[name]) {
+        Model[name].push(modelOptions[name]);
+      }
+    }
+  };
+
+  initHandler('preGet', function preGet (esRequest, callback) {
+    callback(null, _.assign({
+      index: index,
+      type: type
+    }, esRequest));
+  });
+
+  Model.get = modelOptions.get || function get (esRequest, callback) {
+    var preGet0 = function (callback) {
+      callback(null, esRequest);
+    };
+    async.waterfall([preGet0].concat(Model.preGet), function (err, esRequest) {
+      if (err) {
+        return callback(err);
+      }
+
+      client.get(esRequest, function (err, esResponse) {
+        if (err) {
+          return callback(err);
+        }
+
+        var postGet0 = function (callback) {
+          callback(null, esResponse);
+        };
+        var postGet = Model.postGet.map(function (f) {
+          return function (esResponse, cb) {
+            f(esRequest, esResponse, cb);
+          };
+        });
+
+        async.waterfall([postGet0].concat(postGet), function (err, esResponse) {
+          if (err) {
+            return callback(err);
+          }
+
+          callback(null, new Model(esResponse._source, esResponse), esResponse);
+        });
+      });
+    });
+  };
+
+  initHandler('postGet', function postGet (esRequest, esResponse, callback) {
+    callback(null, esResponse);
+  });
+
+  initHandler('preSearch', function preSearch (esRequest, callback) {
+    callback(null, _.assign({
+      index: index,
+      type: type,
+      version: modelOptions.version
+    }, esRequest));
+  });
+
+  Model.search = modelOptions.search || function search (params, callback) {
+    if (arguments.length < 2) {
+      callback = arguments[0];
+      params = null;
+    }
+
+    var preSearch0 = function (callback) { // seed waterfall
+      callback(null, params);
+    };
+
+    async.waterfall([preSearch0].concat(Model.preSearch), function (err, esRequest) {
+      if (err) {
+        return callback(err);
+      }
+
+      client.search(esRequest, function (err, esResponse) {
+        if (err) {
+          return callback(err);
+        }
+
+        var postSearch0 = function (callback) {
+          callback(null, esResponse);
+        };
+        var postSearch = Model.postSearch.map(function (f) {
+          return function (esResponse, cb) {
+            f(esRequest, esResponse, cb);
+          };
+        });
+
+        async.waterfall([postSearch0].concat(postSearch), function (err, esResponse) {
+          if (err) {
+            return callback(err);
+          }
+
+          var results = esResponse.hits.hits.map(function (hit) {
+            return new Model(hit._source, hit);
+          });
+
+          callback(null, results, esResponse);
+        });
+      });
+
+      // TODO emit search event for consumers that want to be informed, but don't want to intercept request?
+    });
+  };
+
+  /**
+   * Called after every search request. Useful to filter responses from elasticsearch.
+   */
+  initHandler('postSearch', function (esRequest, esResponse, callback) {
+    callback(null, esResponse);
+  });
+
+  initHandler('preDelete', function (esRequest, callback) {
+    callback(null, _.assign({
+      index: index,
+      type: type,
+      refresh: refresh
+    }, esRequest));
+  });
+
+  Model.delete = function _delete (esRequest, callback) {
+    if (arguments.length < 2) {
+      callback = arguments[0];
+      esRequest = null;
+    }
+
+    var preDelete0 = function (callback) {
+      callback(null, esRequest);
+    };
+
+    async.waterfall([preDelete0].concat(Model.preDelete), function (err, esRequest) {
+      if (err) {
+        return callback(err);
+      }
+
+      if (esRequest.body || esRequest.q) {
+        // TODO does this make a difference?
+        return client.deleteByQuery(esRequest, callback);
+      } else {
+        return client.delete(esRequest, callback); // TODO pass new model instance?
+      }
+    });
+  };
+
+  initHandler('preInsert', function preInsert (doc, callback) {
+    callback(null, doc);
+  });
+
+  /**
+   * Index a document into elasticsearch. We call this method `insert` to distinguish the operation (verb) from an
+   * elasticsearch index (noun).
+   */
+  Model.prototype.insert = function insert (params, callback) {
+    if (arguments.length < 2) {
+      callback = arguments[0];
+      params = null;
+    }
+    var model = this;
+
+    var preInsert0 = function (callback) {
+      callback(null, model);
+    };
+
+    async.waterfall([preInsert0].concat(Model.preInsert), function (err, doc) {
+      if (err) {
+        return callback(err);
+      }
+
+      params = _.assign({ // clients can override all params
+        index: model._.index, // don't use model.index! That comes from the client
+        type: model._.type, // happy little fields :)
+        id: model._.id,
+        refresh: model._.refresh,
+        body: _.assign({}, doc) // don't include non-enumerable props, just in case
+      }, params);
+
+      // TODO pass new Model instance?
+      return client.index(params, callback);//model._.client.index(params, callback);
+    });
+  };
+
+  _.assign(Model, modelOptions.classMethods);
+  _.assign(Model.prototype, modelOptions.instanceMethods);
+
+  return Model;
 }
 
-/**
- * Called when constraints on a record need to be checked. The default implementation does no constraint checking.
- * Currently only called on index requests.
- */
-Model.prototype.checkConstraints = function (params, callback) {
-  callback(null);
-};
-
-// Bulk operations
-Model.prototype.bulk = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  // Bulk has two modes, but uses the index and type as fallback defaults
-  // so they can always be specified
-  params = _.assign({
-    index: this.index.name,
-    type: this.type,
-    refresh: true // we have a very low write volume, so we favor consistency over speed by default
-  }, params);
-  this.client.bulk(params, callback);
-};
-
-// Delete a single record
-Model.prototype.delete = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  params = _.assign({
-    index: this.index.name,
-    type: this.type,
-    refresh: true
-  }, params);
-  this.client.delete(params, callback);
-};
-
-// Delete by querying the index
-Model.prototype.deleteByQuery = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  params = _.assign({
-    index: this.index.name,
-    type: this.type,
-    refresh: true
-  }, params);
-  this.client.deleteByQuery(params, callback);
-};
-
-// Check if a record exists
-Model.prototype.exists = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  params = _.assign({
-    index: this.index.name,
-    type: this.type
-  }, params);
-  this.client.exists(params, callback);
-};
-
-// Get the contents of a single record
-Model.prototype.get = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  params = _.assign({
-    index: this.index.name,
-    type: this.type
-  }, params);
-  this.client.get(params, callback);
-};
-
-// Insert a record
-Model.prototype.insert = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  params = _.assign({
-    index: this.index.name,
-    type: this.type,
-    refresh: true
-  }, params);
-
-  this.checkConstraints(params, function (err, failure) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    if (failure) {
-      callback(new errors.ConstraintError(failure.name, failure.message));
-    } else {
-      // all good, we can proceed with the write
-      this.client.index(params, callback);
-    }
-  }.bind(this));
-};
-
-// Get the contents of multiple records
-Model.prototype.mget = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  // Mget has two modes - index and type per request, or bulk for a set of ids
-  // Only provide default index and type for the latter case
-  if (params && params.body.ids) {
-    params = _.assign({
-      index: this.index.name,
-      type: this.type
-    }, params);
-  }
-  this.client.mget(params, callback);
-};
-
-// Run a query on the index
-Model.prototype.search = function (params, callback) {
-  if (arguments.length < 2) {
-    callback = arguments[0];
-    params = null;
-  }
-  params = _.assign({
-    index: this.index.name,
-    type: this.type,
-    version: true
-  }, params);
-  this.client.search(params, callback);
-};
-
-module.exports = Model;
+module.exports = model;
--- test/server/codex/test-model.js
+++ test/server/codex/test-model.js
@@ -5,7 +5,8 @@ chai.use(require('sinon-chai'));
 var expect = chai.expect;
 var nock = require('nock');
 
-var Model = require('../../../server/codex/model');
+var _ = require('lodash');
+var codex = require('../../../server/codex');
 var conf = require('../../../server/conf');
 
 describe('model', function () {
@@ -13,42 +14,470 @@ describe('model', function () {
     nock.cleanAll();
   });
 
-  describe('exists()', function () {
-    var scope;
-    var model;
-    beforeEach(function (done) {
-      scope = nock(conf.elasticsearch.host)
-        .head('/foo')
-        .reply(200);
+  it('should add instanceMethods to prototype', function (done) {
+    var Bar = codex.model({
+      index: 'foo',
+      type: 'bar',
+      instanceMethods: {
+        sayHello: function () {
+          return 'hello';
+        }
+      }
+    });
 
-      model = new Model({
+    expect(new Bar({}).sayHello()).to.equal('hello');
+
+    done();
+  });
+
+  it('should add classMethods to class', function (done) {
+    var Bar = codex.model({
+      index: 'foo',
+      type: 'bar',
+      classMethods: {
+        sayHello: function () {
+          return 'hello';
+        }
+      }
+    });
+
+    expect(Bar.sayHello()).to.equal('hello');
+
+    done();
+  });
+
+  it('should set classMethods\' `this` to Model class', function (done) {
+    var Bar = codex.model({
+      index: 'foo',
+      type: 'bar',
+      classMethods: {
+        sayHello: function () {
+          expect(this).to.equal(Bar);
+          done();
+        }
+      }
+    });
+
+    Bar.sayHello();
+  });
+
+  it('instanceMethods should have access to fields', function (done) {
+    var Bar = codex.model({
+      index: 'foo',
+      type: 'bar',
+      instanceMethods: {
+        sayHello: function () {
+          return this.hello;
+        }
+      }
+    });
+
+    expect(new Bar({hello: 'hello'}).sayHello()).to.equal('hello');
+
+    done();
+  });
+
+  describe('insert()', function () {
+    it('should write to elasticsearch', function (done) {
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: 'r@nd0m!d',
+        _version: 1,
+        created: true
+      };
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar', {
+          a: 1
+        })
+        .reply(201, response);
+
+      var Bar = codex.model({
         index: 'foo',
         type: 'bar'
       });
 
-      done();
-    });
-
-    it('should handle optional arguments', function (done) {
-      model.index.exists(function (err) {
+      new Bar({a: 1}).insert(function (err, res) {
         if (err) {
-          done(err);
-          return;
+          return done(err);
         }
 
-        expect(scope.isDone()).to.be.true;
+        expect(res).to.deep.equal(response);
+
         done();
       });
     });
 
-    it('should work if passed null', function (done) {
-      model.index.exists(null, function (err) {
+    it('should respect any id passed to it', function (done) {
+      var a1 = {a: 1};
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: '1',
+        _version: 1,
+        created: true
+      };
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar/1', a1)
+        .reply(201, response);
+
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar'
+      });
+
+      new Bar(a1, {id: 1}).insert(function (err, res) {
         if (err) {
-          done(err);
-          return;
+          return done(err);
         }
 
-        expect(scope.isDone()).to.be.true;
+        expect(res).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should run any preInsert methods', function (done) {
+      var a1 = {a: 1};
+      var b2 = {b: 2};
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: 'r@nd0m!d',
+        _version: 1,
+        created: true
+      };
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar', b2)
+        .reply(201, response);
+
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        preInsert: function (model, callback) {
+          expect(model).to.deep.equal(new Bar(a1));
+          callback(null, new Bar(b2));
+        }
+      });
+
+      new Bar({a: 1}).insert(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should propagate preInsert errors', function (done) {
+      var Bar = codex.model({
+        preInsert: function (model, callback) {
+          callback(new Error());
+        }
+      });
+
+      new Bar().insert(function (err, response) {
+        expect(err).to.be.instanceof(Error);
+        expect(response).to.be.undefined;
+
+        done();
+      });
+    });
+
+    it('should allow passing refresh=true to model definition', function (done) {
+      var a1 = {a: 1};
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: 'r@nd0m!d',
+        _version: 1,
+        created: true
+      };
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        refresh: true
+      });
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar?refresh=true', a1)
+        .reply(201, response);
+
+      new Bar(a1).insert(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should allow passing refresh=true to model instance', function (done) {
+      var a1 = {a: 1};
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: 'r@nd0m!d',
+        _version: 1,
+        created: true
+      };
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        refresh: true
+      });
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar?refresh=true', a1)
+        .reply(201, response);
+
+      new Bar(a1, {refresh: true}).insert(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should allow passing refresh=true to insert', function (done) {
+      var a1 = {a: 1};
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: 'r@nd0m!d',
+        _version: 1,
+        created: true
+      };
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        refresh: true
+      });
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar?refresh=true', a1)
+        .reply(201, response);
+
+      new Bar(a1).insert({refresh: true}, function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res).to.deep.equal(response);
+
+        done();
+      });
+    });
+  });
+
+  describe('search()', function () {
+    it('should search elasticsearch', function (done) {
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar'
+      });
+      var response = {
+        took: 5,
+        'timed_out': false,
+        hits: {
+          total: 0,
+          'max_score': null,
+          hits: []
+        }
+      };
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar/_search?q=a')
+        .reply(200, response);
+
+      Bar.search({q: 'a'}, function (err, bars, esResponse) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(bars).to.deep.equal([]);
+        expect(esResponse).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should call preSearch', function (done) {
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        preSearch: function (esRequest, callback) {
+          expect(esRequest.q).to.equal('a');
+          callback(null, _.assign({}, esRequest, {q: 'b'}));
+        }
+      });
+      var response = {
+        took: 5,
+        'timed_out': false,
+        hits: {
+          total: 0,
+          'max_score': null,
+          hits: []
+        }
+      };
+
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar/_search?q=b')
+        .reply(200, response);
+
+      Bar.search({q: 'a'}, function (err, bars, esResponse) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(bars).to.deep.equal([]);
+        expect(esResponse).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should call postSearch', function (done) {
+      var searchReq = {q: 'a'};
+      var response = {
+        took: 5,
+        'timed_out': false,
+        hits: {
+          total: 0,
+          'max_score': null,
+          hits: []
+        }
+      };
+      var postResponse = {
+        hits: {
+          hits: [
+            {
+              _source: {
+                a: 1
+              }
+            }
+          ]
+        }
+      };
+      nock(conf.elasticsearch.host)
+        .post('/foo/bar/_search?q=a')
+        .reply(200, response);
+
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        postSearch: function (request, resp, callback) {
+          expect(resp).to.deep.equal(response);
+
+          callback(null, postResponse);
+        }
+      });
+
+      Bar.search(searchReq, function (err, bars, resp) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(bars.length).to.equal(1);
+        expect(resp).to.deep.equal(postResponse);
+
+        done();
+      });
+    });
+  });
+
+  describe('get()', function () {
+    it('should return document', function (done) {
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: '1',
+        found: true,
+        _source: {}
+      };
+      nock(conf.elasticsearch.host)
+        .get('/foo/bar/1')
+        .reply(200, response);
+
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar'
+      });
+
+      Bar.get({id: 1}, function (err, bar, resp) {
+        expect(bar._.id).to.equal('1');
+        // TODO check enumberable props
+        expect(resp).to.deep.equal(response);
+
+        done();
+      });
+    });
+
+    it('should return a model instance', function (done) {
+      var response = {
+        _index: 'foo',
+        _type: 'bar',
+        _id: '1',
+        found: true,
+        _source: {}
+      };
+      nock(conf.elasticsearch.host)
+        .get('/foo/bar/1')
+        .reply(200, response);
+
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar',
+        instanceMethods: {
+          getId: function () {
+            return this._.id;
+          }
+        }
+      });
+
+      Bar.get({id: 1}, function (err, bar) {
+        expect(bar.getId).to.exist;
+        expect(bar.getId()).to.equal('1');
+
+        done();
+      });
+    });
+  });
+
+  describe('delete()', function () {
+    it('should delete given ID', function (done) {
+      var response = {
+        found: true,
+        _index: 'foo',
+        _type: 'bar',
+        _id: '1',
+        _version: 2
+      };
+      nock(conf.elasticsearch.host)
+        .delete('/foo/bar/1')
+        .reply(200, response);
+
+      var Bar = codex.model({
+        index: 'foo',
+        type: 'bar'
+      });
+
+      Bar.delete({id: 1}, function (err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        expect(res).to.deep.equal(response);
+
         done();
       });
     });
