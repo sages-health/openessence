7c26dec812954fd1ea4982243e15acd6a4a994a5
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-06-17 10:41:02 -0400

    Track changes to documents with CaperTrail
    Inspired by the popular PaperTrail gem for ActiveRecord, CaperTrail is a
    paper trail for Codex. So far, it only records the date and (optionally)
    the user that modified a record, but eventually it might gain more cool
    features.

---
+++ server/caper-trail/README.md
@@ -0,0 +1,3 @@
+# CaperTrail - a paper trail for Codex
+
+Inspired by ActiveRecord's [PaperTrail](https://github.com/airblade/paper_trail).
---
+++ server/caper-trail/index.js
@@ -0,0 +1,84 @@
+'use strict';
+
+var _ = require('lodash');
+var logger = require('../conf').logger;
+
+// Inspired by ActiveRecord's PaperTrail: https://github.com/airblade/paper_trail
+// Currently only records when updates happen
+// TODO import more features from PaperTrail, like recording old versions of documents
+
+function makePaperTrail (model, callback) {
+  var createEntry = function () {
+    return {
+      createdAt: new Date()
+    };
+  };
+
+  if (model._.id) {
+    // get list of modifications
+    model._.get({id: model._.id}, function (err, instance) {
+      if (err) {
+        return callback(err);
+      }
+
+      var trail = instance.paperTrail || [];
+      trail.push(createEntry());
+
+      return callback(null, trail);
+    });
+  } else {
+    return callback(null, [createEntry()]);
+  }
+}
+
+function caperTrailModel (Model) {
+  Model.preInsert.push(function paperTrail (model, callback) {
+    if (model.paperTrail) {
+      // models allow consumers to override paper trails, controllers don't
+      return callback(null, model);
+    }
+
+    makePaperTrail(model, function (err, trail) {
+      if (err) {
+        return callback(err);
+      }
+
+      // This allows consumers to override paperTrail if they need to. The controller blocks untrusted clients from
+      // being able to do this maliciously
+      callback(null, _.assign({paperTrail: trail}, model));
+    });
+  });
+
+  return Model;
+}
+
+function caperTrailController (controller) {
+  if (!controller.preInsert) {
+    logger.warn('Cannot use CaperTrail on a controller that doesn\'t support insert');
+    return;
+  }
+
+  controller.preInsert.push(function paperTrail (req, esRequest, callback) {
+    if (esRequest.body.paperTrail) {
+      logger.warn({req: req}, 'Client tried to overwrite paper trail. Don\'t worry, we got \'em');
+    }
+
+    makePaperTrail(new controller.Model(esRequest.body, esRequest), function (err, trail) {
+      if (err) {
+        return callback(err);
+      }
+
+      // this is why it's important not to keep around extra properties (like passwords) on req.user
+      trail[trail.length - 1].user = req.user;
+
+      // the order of these assigns is crucial - this way we overwrite any paperTrails the client tried to send
+      var body = _.assign({}, esRequest.body, {paperTrail: trail});
+      callback(null, _.assign({}, esRequest, {body: body}));
+    });
+  });
+}
+
+module.exports = {
+  model: caperTrailModel,
+  controller: caperTrailController
+};
