3ba20c5a25c39cc59686e4b90152ad846ec90b8b
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-07-03 18:33:32 -0400

    Clean up Windows service code

--- gulpfile.js
+++ gulpfile.js
@@ -433,19 +433,27 @@ gulp.task('cert', function (done) {
   });
 });
 
+// Install Fracas as a service so that it starts automatically on boot. If anything goes wrong on Windows,
+// you can delete the service with `sc delete fracas.exe`, which should be run as an admin.
 gulp.task('service', function (done) {
+  if (require('os').platform() !== 'win32') {
+    // TODO install launchd/upstart/systemd/SMF/SysV script on *nix platforms
+    return done(new Error('Sorry, we only support installing a Windows service right now'));
+  }
+
   var Service;
   try {
     Service = require('node-windows').Service;
   } catch (e) {
-    gutil.log(gutil.colors.red('Couldn\'t load node-windows. Did you run `npm install -g node-windows` and `npm link node-windows`?'));
+    gutil.log(gutil.colors.red('Couldn\'t load node-windows. Did you run ' +
+      '`npm install -g node-windows && npm link node-windows`?'));
     return done(e);
   }
 
   var svc = new Service({
     name: 'Fracas',
     description: 'Disease surveillance webapp',
-    script: require('path').join(__dirname,'server.js'),
+    script: path.join(__dirname, 'server.js'),
     env: [
       {
         name: 'NODE_ENV',
@@ -457,7 +465,7 @@ gulp.task('service', function (done) {
     gutil.log('Service started successfully. Happy Fracasing :)');
     done();
   });
-  svc.on('install',function () {
+  svc.on('install', function () {
     svc.start();
   });
 
