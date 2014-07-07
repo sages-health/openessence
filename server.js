864514b7b246fa64fd44b70ec80a8936137daa97
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-07-07 17:18:59 -0400

    Don't cluster if we're debugging

--- server.js
+++ server.js
@@ -14,6 +14,20 @@ if (module.parent) {
   throw new Error('server.js must be run in its own process');
 }
 
+/**
+ *
+ * @param [callback] - Optional callback
+ */
+var startServer = function (callback) {
+  if (conf.ssl.enabled) {
+    https.createServer({key: conf.ssl.key, cert: conf.ssl.cert}, app)
+      .listen(conf.ssl.port);
+  }
+
+  // If we're running behind TLS, then our HTTP server just redirects to HTTPS
+  http.createServer(app).listen(conf.httpPort, callback);
+};
+
 if (cluster.isMaster) {
   logger.info('Running in %s mode', conf.env);
 
@@ -39,13 +53,7 @@ if (cluster.isMaster) {
     logger.info('Skipping PhantomJS');
   }
 
-  logger.info('Forking %d Fracas workers from parent process %d', conf.workers, process.pid);
-
-  cluster.on('online', function (worker) {
-    logger.info('Worker %d is now online', worker.process.pid);
-  });
-
-  cluster.once('listening', function () {
+  var onServerListening = function () {
     // This message isn't just to be friendly. We really only support running Fracas at one URL, since it makes
     // a lot of things, e.g. redirects, a lot easier. This log statement helps nudge the admin into using the preferred
     // URL.
@@ -58,7 +66,37 @@ if (cluster.isMaster) {
         url: conf.url
       });
     }
+  };
+
+  var debug = process.execArgv.some(function (arg) {
+    // Regex taken from https://github.com/joyent/node/commit/43ec1b1c2e77d21c7571acd39860b9783aaf5175
+    return /^(--debug|--debug-brk)(=\d+)?$/.test(arg);
   });
+
+  // Debugging is annoying with multiple processes, since even if you give each worker a separate debug port, you need
+  // to pick one to connect to. It's easiest to just not fork when we're debugging.
+  // See https://github.com/joyent/node/issues/5318
+  if (debug) {
+    logger.warn('Disabling clustering in debug mode');
+
+    return startServer(function (err) {
+      if (err) { // I don't think listen can return an error...
+        return logger.error(err);
+      }
+
+      onServerListening();
+    });
+  }
+
+  // otherwise, we're using clustering
+
+  logger.info('Forking %d Fracas workers from parent process %d', conf.workers, process.pid);
+
+  cluster.on('online', function (worker) {
+    logger.info('Worker %d is now online', worker.process.pid);
+  });
+
+  cluster.once('listening', onServerListening);
 
   cluster.on('exit', function (worker) {
     logger.warn('Worker %d died. Restarting now...', worker.process.pid);
@@ -70,12 +108,5 @@ if (cluster.isMaster) {
   }
 } else {
   // cluster workers
-
-  if (conf.ssl.enabled) {
-    https.createServer({key: conf.ssl.key, cert: conf.ssl.cert}, app)
-      .listen(conf.ssl.port);
-  }
-
-  // If we're running behind TLS, then our HTTP server just redirects to HTTPS
-  http.createServer(app).listen(conf.httpPort);
+  startServer();
 }
