e57eb7f437ad8669a98656cd7ac042e540974d21
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-07-08 17:29:59 -0400

    Delete geometry model

--- server/models/geometry.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var codex = require('../codex');
-
-module.exports = codex.model({
-  index: 'region',
-  type: 'district',
-  refresh: true
-}).with(require('../caper-trail').model);
