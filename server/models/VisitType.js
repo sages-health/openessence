244cec783024b2ab21cb3e046bb2daf1a1ee2840
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-09-12 16:34:54 -0400

    Consolidate our indices
    Before this commit, we had a lot of indices that stored small amounts
    of data, often only a few documents. This is annoying to maintain and
    drove our hosting costs up (a lot of elasticsearch providers charge per
    index).
    
    This commit consolidates all our indices into two: `fracas_data` and
    `fracas_settings`, which are actually aliases to timestamped indices.
    This should make remapping less painful.

--- server/models/VisitType.js
+++ server/models/VisitType.js
@@ -4,8 +4,8 @@ var codex = require('../codex');
 var conf = require('../conf');
 
 module.exports = codex.model({
-  index: 'visit_type',
-  type: 'visit_type',
+  index: 'fracas_settings',
+  type: 'visit',
   refresh: true,
   client: conf.elasticsearch.client
 }).with(require('../caper-trail').model);
