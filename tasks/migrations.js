33481b7ee3c631fc833a5d97d0263e4a9e4f0640
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2014-12-11 10:54:06 -0500

    Fix spurious es connection log statements

--- tasks/migrations.js
+++ tasks/migrations.js
@@ -1,12 +1,15 @@
 'use strict';
 
-
 var gulp = require('gulp');
 
-var clean = require('../server/migrations/clean');
-var reseed = require('../server/migrations/reseed');
-
-gulp.task('es-clean', clean);
+gulp.task('es-clean', function (done) {
+  // eagerly requiring clean causes spurious elasticsearch connection logs
+  var clean = require('../server/migrations/clean');
+  clean(done);
+});
 
 // Delete all your Fracas data and reseed. Obviously, this should be run with extreme caution.
-gulp.task('reseed', ['es-clean'], reseed);
+gulp.task('reseed', ['es-clean'], function (done) {
+  var reseed = require('../server/migrations/reseed');
+  reseed(done);
+});
