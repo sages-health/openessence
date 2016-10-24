b7992351a8fdd54c6327c8b738081a4ebd872698
Miles Stewart <miles.stewart@jhuapl.edu>
2016-10-24 16:03:30 -0400

    Example settings for dev

--- config/settings.example.js
+++ config/settings.example.js
@@ -9,6 +9,10 @@ module.exports = function (settings) {
     }
   };
 
+  settings.MAP_URL = "'http://localhost:8080/styles/bright-v9/?vector#{z}/{x}/{y}'"
+  settings.MAP_LATITUDE = '41.4925'
+  settings.MAP_LONGITUDE = '-99.9018'
+
   settings.session.secret = '!superDuperSecretSe$$i@nKey!';
 
   return settings;
